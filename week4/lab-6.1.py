9ef5e02148bf5864ffb7596a86ef5186e2d1029f
lucyfinnerty <114194409+lucyfinnerty@users.noreply.github.com>
2024-07-05 12:16:26 -0500

    completed and working

--- week4/lab-6.1.py
+++ week4/lab-6.1.py
@@ -10,19 +10,24 @@ Date: 7/2/24
 
 import math
 
+# function that calculates a cylinder's volume based on its radius and length
 def cylVol (radius, length):
-    volume = math.pi * (radius * radius) * length
+    volume = math.pi * (radius * radius) * length # volume equation --> 3.14 * r^2 * L
     return volume
 
+# function that calculates a cylinder's side surface area based on its radius and length
 def surfArea (radius, length):
-    surfArea = 2 * math.pi * radius * length
+    surfArea = 2 * math.pi * radius * length # surface area of side equation --> 2 * 3.14 * r * L
     return surfArea
 
+# ask user for radius and length of their cylinder for calculations
 radius = eval(input("Enter radius of cylinder: "))
 length = eval(input("Enter length of cylinder: "))
 
+# variables volume and surfArea are set to the final calculated number that is returned from the funtions called
 volume = cylVol(radius, length)
 surfArea = surfArea(radius, length)
 
+# displays final outputs to user
 print("The volume of your cylinder is", volume)
 print("The side surface area of your cylinder is", surfArea)
